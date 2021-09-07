3301b4041f959b633c47e36597e4cb6a22a9dfd5
SumedhZope <sumedhzope@gmail.com>
2021-09-07 19:07:18 +0530

    Parallel processing now working!!

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os,discord,pytube
+from discord.utils import get
 from dotenv import load_dotenv
 from mega import Mega
 import _thread as trd
+import threading
 import time
-
-from requests.api import delete
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,7 +18,7 @@ new_mega = mega.login(os.getenv('MEGA_USR'), os.getenv('MEGA_PWD'))
 
 #global required variables
 time_limit = 600
-delete_time = 3600
+delete_time = 3600   
 
 @client.event
 async def on_ready():
@@ -33,8 +34,8 @@ async def on_message(message):
     if clean_content[0] == "!ping": #respond command to test bot
         await message.reply("PONG")
 
-    if clean_content[0] == "!get": #Get complete video (!get link)
-
+    if clean_content[0] == "!get": #Get complete video (!get link) 
+        
         try: 
             yt = pytube.YouTube(clean_content[1])
 
@@ -42,16 +43,10 @@ async def on_message(message):
                 await message.channel.send(f"Length of given video exceeds {time_limit//60} minutes, which is currently unsupported.")
                 return 
 
-            yt.streams.get_highest_resolution().download(output_path="./videos",filename=f"down_{message.author}")
+            get_func_thread = threading.Thread(target=get_func, args=(message, yt))
+            get_func_thread.start()
 
-            main_folder = new_mega.find('discord_videos')
-            file = new_mega.upload(f"./videos/down_{message.author}", main_folder[0])
-
-            await message.reply(f"{new_mega.get_upload_link(file)}")
-
-            os.remove(f"./videos/down_{message.author}")
-
-            return
+            return       
 
         except:
             await message.reply("Some error occured :(")
@@ -88,22 +83,12 @@ async def on_message(message):
             if yt.length > time_limit:
                 await message.channel.send(f"Length of given video exceeds {time_limit//60} minutes, which is currently unsupported.")
                 return 
-                
-            yt.streams.get_highest_resolution().download(output_path="./videos",filename=f"down_{message.author}")
-            os.system(f"ffmpeg -i ./videos/down_{message.author} -ss {start_time} -to {end_time} ./videos/down_{message.author}_temp.mp4")
 
-            main_folder = new_mega.find('discord_videos')
-            
-            file = new_mega.upload(f"videos/down_{message.author}_temp.mp4", main_folder[0])
-
-            await message.reply(f"File is only valid for {delete_time//3600} hour(s), link : {new_mega.get_upload_link(file)}")
-
-            os.remove(f"./videos/down_{message.author}")
-            os.remove(f"./videos/down_{message.author}_temp.mp4")
-            
-            trd.start_new_thread(test_func, (file, ))
+            clip_func_thread = threading.Thread(target=clip_func, args=(message, yt, start_time, end_time))
+            clip_func_thread.start()
 
             return
+
         except:
             await message.channel.send(f"Some error occured :(")
             return
@@ -112,5 +97,32 @@ def test_func(file):
     time.sleep(delete_time)
     new_mega.delete(f"{new_mega.get_id_from_obj(file)}")
 
+def get_func(message,yt):
+
+    yt.streams.get_highest_resolution().download(output_path="./videos",filename=f"down_{message.author}")
+
+    main_folder = new_mega.find('discord_videos')
+    file = new_mega.upload(f"./videos/down_{message.author}", main_folder[0])
+    os.remove(f"./videos/down_{message.author}")
+    
+    print(f"processed {message.author}")
+
+    client.loop.create_task(message.reply(f"File is only valid for {delete_time//3600} hour(s), link : {new_mega.get_upload_link(file)}"))
+
+def clip_func(message, yt, start_time, end_time):
+    yt.streams.get_highest_resolution().download(output_path="./videos",filename=f"down_{message.author}")
+    os.system(f"ffmpeg -i ./videos/down_{message.author} -ss {start_time} -to {end_time} ./videos/down_{message.author}_temp.mp4")
+
+    main_folder = new_mega.find('discord_videos')
+    
+    file = new_mega.upload(f"videos/down_{message.author}_temp.mp4", main_folder[0])
+
+    os.remove(f"./videos/down_{message.author}")
+    os.remove(f"./videos/down_{message.author}_temp.mp4")
+    
+    delete_thread = threading.Thread(target=test_func, args=(file))
+    delete_thread.start()
+
+    client.loop.create_task(message.reply(f"File is only valid for {delete_time//3600} hour(s), link : {new_mega.get_upload_link(file)}")) 
 
 client.run(TOKEN)
